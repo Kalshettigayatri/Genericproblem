19492ef355909be1a0e4bd78a0f387dc66d0aa8e
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-12 18:02:47 +0530

    Welcome to generic program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Genericproblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Genericproblem");
+        }
+    }
+}
